0832beafaf869dad9a13a10c7c6a249d69f3817a
Peter Organisciak <organisciak@gmail.com>
2019-11-04 17:51:13 -0700

    Add chunking and case options to convert script

--- scripts/convert-to-parquet.py
+++ scripts/convert-to-parquet.py
@@ -8,7 +8,12 @@ def main():
     
     parser.add_argument('--efdir', type=str, default='/data/extracted-features/', help='Location of the EF files')
     parser.add_argument('--outdir', type=str, default='/data/extracted-features-parquet/', help='Output location for parquet files.')
+    
+    parser.add_argument('--chunked', action='store_true', help='Whether to chunk the internal tokenlist.')
+    parser.add_argument('--page-ref', action='store_true', help='Store page reference when chunking.')
+    parser.add_argument('--chunk-size', type=int, default=5000, help='Word target for chunks.')
 
+    parser.add_argument('--lowercase', action='store_true', help='Lowercase tokens.')
     parser.add_argument('filepaths', type=str, nargs='+',  help='files to convert')
     
     args = parser.parse_args()
@@ -21,7 +26,12 @@ def main():
             path, filename = os.path.split(path)
 
             os.makedirs(path, exist_ok=True)
-            vol.save_parquet(path, token_kwargs=dict(section='body', drop_section=True, pos=False))
+            token_kwargs=dict(section='body', drop_section=True, pos=False, 
+                              case=(not args.lowercase))
+            if args.chunked:
+                token_kwargs['chunk_target'] = args.chunk_size
+                token_kwargs['page_ref'] = args.page_ref
+            vol.save_parquet(path, chunked=args.chunked, token_kwargs=token_kwargs)
         except:
             with open('errs.txt', mode='a') as f:
                 f.write(efpath + "\n")
