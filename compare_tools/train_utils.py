ed9e02a00032945d5f672aa8a4406de6c2dd9116
Peter Organisciak <organisciak@gmail.com>
2020-04-27 12:08:47 -0600

    Add TFRecord export for similarity matrix

---
+++ compare_tools/train_utils.py
@@ -0,0 +1,112 @@
+import numpy as np
+import tensorflow as tf
+
+judgment_labels = ['SWSM', 'SWDE', 'WP_DV', 'PARTOF', 'CONTAINS', 'AUTHOR', 'DIFF']
+
+def df_to_tfrecords(X, output_file, labels='default'):
+    '''
+    Take a dataframe where
+        - the first X*X columns are features for a pairwise comparison between
+    two books, up to X chunks, with extra chunks padded with zeros. X doesn't need
+    to be known here - the assumption is that everything but the last four columns
+    is a feature.
+        - the final four columns have 'left', 'right', 'judgment', and 'notes'
+        
+    This is saved to a TFRecord file, a Proto-based format that works well with Tensorflow.
+    
+    Judgment labels are converted to an integer and then one-hot encoded. To safely keep a consistent
+    label->label_int key, they are hardcoded. Change only for your own use cases. If adding
+    classes for the present problem, add to the end of the current list. Just in case, the
+    string judgment is still kept in the TFRecord file.
+    '''
+    
+    if labels == 'default':
+        labels = judgment_labels
+    # Create a {code: index} dict
+    label_ref = dict(zip(judgment_labels, range(len(judgment_labels))))
+    
+    writer = tf.io.TFRecordWriter(output_file)
+    for i, row in X.iterrows():
+        # Convert judgment to one-hot encode
+        if 'judgment' in row and row['judgment'] in label_ref:
+            y = tf.one_hot(label_ref[row['judgment']], len(labels))
+        else:
+            y = tf.one_hot(-1, len(labels))
+            
+        feature = {
+          'X': tf.train.Feature(float_list=tf.train.FloatList(value=row.iloc[:-4].values.flatten())),
+          'left': tf.train.Feature(bytes_list=tf.train.BytesList(value=[row['left'].encode('utf-8')])),
+          'right': tf.train.Feature(bytes_list=tf.train.BytesList(value=[row['right'].encode('utf-8')])),
+          'judgment': tf.train.Feature(bytes_list=tf.train.BytesList(value=[row['judgment'].encode('utf-8')])),
+          'y': tf.train.Feature(int64_list=tf.train.Int64List(value=y)),
+          'notes': tf.train.Feature(bytes_list=tf.train.BytesList(value=[row['notes'].encode('utf-8')])),
+        }
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+        serialized = example.SerializeToString()
+        writer.write(serialized)
+
+    writer.close()
+
+def parse_comparison_records(example_proto, labels='default', input_shape=(50,50,1), parse_single=False):
+    '''Definition for reading TFRecords. Input shape should be three-dimensional, with a channel at the end.'''
+    if labels == 'default':
+        labels = judgment_labels
+    features = {
+        'X': tf.io.FixedLenFeature(input_shape, tf.float32),
+        'left': tf.io.FixedLenFeature([], tf.string),
+        'right': tf.io.FixedLenFeature([], tf.string),
+        'judgment': tf.io.FixedLenFeature([], tf.string),
+        'y': tf.io.FixedLenFeature([len(labels)], tf.int64),
+        'notes': tf.io.FixedLenFeature([], tf.string)
+    }
+    '''  parse_example gives a "Shape must be rank 1" error on some versions of tensorflow. You can change
+    to parse_single_example, but it's more transparent to just batch the data first , even if
+    it is a batch of 1; e.g. ds = ds.batch(1)
+    
+    '''
+    if parse_single:
+        parsed_features = tf.io.parse_single_example(example_proto, features)
+    else:
+        parsed_features = tf.io.parse_example(example_proto, features)
+    return parsed_features
+
+def processStats(stats, remove_cols=['LSize', 'RSize', 'minSize']):
+    '''Add derivative columns to stats'''
+    stats = stats.copy()
+    replaced_cols = remove_cols
+    sw_sizes = [int(col[2:-3])/100 for col in stats.columns if col.startswith('SW')]
+    for t in sw_sizes:
+        for s in ['L', 'R']:
+            stats[s+'SW{:04.0f}Prop'.format(t*100)] = stats['SW{:04.0f}Len'.format(t*100)] / stats[s+'Size']
+        replaced_cols.append('SW{:04.0f}Len'.format(t*100))
+            
+    # Log Transforms, robust scale, no +1
+    to_transform_cols = ['gloveMeanSim', 'gloveLMeanMinSim', 'gloveRMeanMinSim', 'gloveLTruncSim', 'gloveRTruncSim']
+    to_transform_cols += [col for col in stats.columns if 'Quantile' in col]
+    for col in to_transform_cols:
+        #To update: Newer versions of daskml have a robust scaler
+        #stats[col+'Transform'] = preprocessing.robust_scale(stats[col].apply(np.log))
+        stats[col+'Transform'] = stats[col].add(.1**7).apply(np.log)
+    replaced_cols += to_transform_cols
+    
+    stats['propSize'] = stats['LSize'] / stats['RSize']
+    
+    # Return a dataframe with the new important stats cols, and another with the ones that have been superceded
+    a = stats.loc[:,[col for col in stats.columns if col not in replaced_cols]]
+    a = stats.loc[:, ['left', 'right'] + [col for col in a.columns if col not in ['left', 'right']]]
+    b = stats.loc[:,list(set(replaced_cols))]
+    return (a, b)
+
+def print_most_important_for_forest(forest, cols):
+    print('most important')
+    # from https://scikit-learn.org/stable/auto_examples/ensemble/plot_forest_importances.html#sphx-glr-auto-examples-ensemble-plot-forest-importances-py
+    importances = forest.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+                 axis=0)
+    indices = np.argsort(importances)[::-1]
+
+    # Print the feature ranking
+    print("Feature ranking:")
+
+    for f in range(X.shape[1]):
+        print("%d. %s (%f)" % (f + 1, cols[indices[f]], importances[indices[f]]))
--- scripts/crunch_stats.py
+++ scripts/crunch_stats.py
@@ -2,6 +2,8 @@ from compare_tools.hathimeta import HathiMeta, get_json_meta
 from compare_tools.configuration import config, init_htid_args
 from compare_tools.comparison import Comparison, HTIDComparison
 from compare_tools.utils import HTID
+import logging
+from compare_tools.train_utils import df_to_tfrecords
 import time
 import os
 import pandas as pd
@@ -9,18 +11,33 @@ import argparse
 import json
 
 def main():
+    logging.basicConfig(level=logging.INFO)
+    
     htid_args = init_htid_args(config)
 
-    parser = argparse.ArgumentParser(description='Take a CSV of left/right judgments to crunch statistics for.')
+    parser = argparse.ArgumentParser(description='Take a set of left/right judgments to crunch statistics for.')
     parser.add_argument('--outdir', default='/data/saddl/stats', type=str,
                         help='Directory to save the file of statistics to.')
-    parser.add_argument('--filename', default="stats-{}.parquet".format(time.time()), type=str,
-                        help='Name of savefile.')
-    parser.add_argument('input_dicts', nargs='+', type=str, 
+    parser.add_argument('--filename', default="stats-{}".format(time.time()), type=str,
+                        help='Name of savefile. Extension automatically added.')
+    parser.add_argument('--tfrecord', action='store_true', help='Whether to save output as a TFRecord.')
+    parser.add_argument('--save-sim', action='store_true',
+                        help='Save an unrolled similarity matrix instead of the statistics.')
+    parser.add_argument('input_dicts', nargs='+', type=str,
                         help="dicts of format {'left':'x', 'right': 'y'} to process. If you're splitting up external data, it's "
-                        "more efficient to sort by 'left' columns first")
+                        "more efficient to sort by 'left' columns first. If you're saving from a DataFrame with 'left' and 'right' "
+                        "columns"", you can convert to the format with df.to_dict(orient='records'). You can also add 'judgment' and 'notes' "
+                        "to the input data if exporting a TFRecord for training."
+                       )
 
     args = parser.parse_args()
+
+    if args.tfrecord:
+        if not args.save_sim:
+            raise Exception('TFRecord can only save a padded similarity matrix.')
+        args.filename = args.filename + '.tfrecord'
+    else:
+        args.filename = args.filename + '.parquet'
 
     outpath = os.path.join(args.outdir, args.filename)
     stats_collector = []
@@ -33,26 +50,42 @@ def main():
     print(len(args.input_dicts))
     for leftid, matches in df.groupby('left'):
         if i % 1000 == 0:
-            print(i, len(stats_collector), leftid, (time.time()-start)/60)
+            logging.info("{}, {}, {}, {}".format(i, len(stats_collector), leftid, (time.time()-start)/60))
         left = HTID(leftid, **htid_args)
 
-        for rightid in matches['right']:
+        for i, match in matches.iterrows():
+            rightid = match['right']
             right = HTID(rightid, **htid_args)
             comp = HTIDComparison(left, right)
             try:
-                stats = comp.all_stats()
-                stats.update(comp.stat_quantiles('srp'))
-                stats.update(comp.stat_quantiles('glove'))
-                stats_collector.append(stats)
+                if args.save_sim:
+                    vec = comp.unrolled_sim()
+                    stats = dict(zip([str(i) for i in range(vec.shape[0])], vec))
+                    stats['left'] = leftid
+                    stats['right'] = rightid
+                    if args.tfrecord:
+                        stats['judgment'] = match['judgment'] if 'judgment' in match else 'UNKNOWN'
+                        stats['notes'] = match['notes'] if 'notes' in match else ''
+                    stats_collector.append(stats)
+                else:
+                    stats = comp.all_stats()
+                    stats.update(comp.stat_quantiles('srp'))
+                    stats.update(comp.stat_quantiles('glove'))
+                    stats_collector.append(stats)
             except KeyboardInterrupt:
                 raise
             except:
-                print("Issue with ", left.htid, right.htid)
+                logging.warning("Issue with {} v. {}".format(left.htid, right.htid))
                 continue
         i += 1
 
-    pd.DataFrame(stats_collector).to_parquet(outpath, compression='snappy')
-    print((time.time() - start)/60)
+    df = pd.DataFrame(stats_collector)
+    if args.tfrecord:
+        df_to_tfrecords(df, outpath)
+    else:
+        df.to_parquet(outpath, compression='snappy')
+    logging.info("{} records parsed in {}min".format(len(df), 
+                                                   ((time.time() - start)/60)))
     
 if __name__ == '__main__':
     main()
